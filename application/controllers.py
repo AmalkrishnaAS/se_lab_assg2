c2de494b056d97d730cefb9b95fcb27e110db470
Amalkrishna A S <amalkrishna02016@gmail.com>
2023-01-22 11:09:49 +0530

    init

---
+++ application/controllers.py
@@ -0,0 +1,90 @@
+from flask import request, redirect, render_template, url_for, flash, session
+import re
+from passlib.hash import sha256_crypt
+from functools import wraps
+
+from flask import current_app as app
+
+from application.models import *
+
+def login_required(f):
+    @wraps(f)
+    def wrap(*args, **kwargs):
+        if 'status' in session:
+            return f(*args, **kwargs)
+        else:
+            flash('Unauthorized Access','danger')
+            return redirect(url_for('login'))
+    return wrap
+
+
+@app.route('/')
+def main():
+    return redirect(url_for('login'))
+
+@app.route('/login', methods=['GET','POST'])
+def login():
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+        
+        
+        if request.form.get('User') == 'Buyer':
+            user = User.query.filter_by(email=email).first()
+        elif request.form.get('User') == 'Vendor':
+            user = Vendors.query.filter_by(email=email).first()
+        else:
+            user = None
+
+        if user is None:
+            flash('Non Existent User','danger')
+            return redirect(url_for('login'))
+        
+        if sha256_crypt.verify(password,user.password):
+            session['status'] = True
+            session['username'] = user.name
+            
+            flash('User Logged In','success')
+            return redirect(url_for('home'))
+        else:
+            flash('Invalid Password','danger')
+            return redirect(url_for('login'))
+    else:
+        return render_template('login.html')
+
+@app.route('/register', methods=['GET','POST'])
+def register():
+    if request.method == 'POST':
+        name = request.form['name']
+        email = request.form['email']
+        password = sha256_crypt.encrypt(request.form['password'])
+        phone = request.form['phone']
+        category = request.form.get('User')
+        
+        if category == 'Buyer':
+            if email.split('@')[1] != "nitc.ac.in":
+                flash('Enter a Valid NITC Mail ID','danger')
+                return redirect(url_for('register'))
+            user = User(name=name, email=email, password=password, phone=phone)
+            db.session.add(user)
+            db.session.commit()
+            flash('User Registered Successfully','success')
+            return redirect(url_for('login'))
+        
+        elif category == 'Vendor':
+            vendor = Vendors(name=name, email=email, password=password, phone=phone)
+            db.session.add(vendor)
+            db.session.commit()
+            flash('Vendor Registered Successfully','success')
+            return redirect(url_for('login'))
+        
+        else:
+            flash('Enter type of User','danger')
+            return redirect(url_for('register'))
+        
+    else:
+        return render_template('register.html')
+    
+@app.route('/home')
+def home():
+    return "success,success!!"
---
+++ application/models.py
@@ -0,0 +1,40 @@
+from application.database import db
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(255), nullable=False)
+    email = db.Column(db.String(255), nullable=False, unique=True)
+    password = db.Column(db.String(255), nullable=False)
+    phone = db.Column(db.String(10), nullable=False)
+
+
+class Vendors(db.Model):
+    __tablename__ = 'vendors'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(255), nullable=False)
+    email = db.Column(db.String(255), nullable=False, unique=True)
+    phone = db.Column(db.String(10), nullable=False)
+    password = db.Column(db.String(255), nullable=False)
+
+    
+class Products(db.Model):
+    __tablename__ = 'products'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(255), nullable=False)
+    vendor = db.Column(db.Integer, db.ForeignKey('vendors.id'), nullable=False)
+    image = db.Column(db.String(255), nullable=False)
+    category = db.Column(db.String(255), nullable=False)
+    price = db.Column(db.Integer, nullable=False)
+    qty = db.Column(db.Integer, nullable=False)
+    unit = db.Column(db.String(255), nullable=False)
+
+class Orders(db.Model):
+    __tablename__ = 'orders'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    vendor = db.Column(db.Integer, db.ForeignKey('vendors.id'), nullable=False)
+    user = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    product = db.Column(db.Integer, db.ForeignKey('products.id'), nullable=False)
+    qty = db.Column(db.Integer, nullable=False)
+    price = db.Column(db.Integer, nullable=False)
+    state = db.Column(db.String(255), nullable=False)
